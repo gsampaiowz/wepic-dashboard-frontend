47ba3d1d569232ce461b4e0070884c264d780ace
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-12-09 17:51:23 -0300

    Tooltip atualizado

--- src/pages/Equipe/MembroCard/MembroCard.jsx
+++ src/pages/Equipe/MembroCard/MembroCard.jsx
@@ -4,12 +4,18 @@ import "./MembroCard.css";
 
 const MembroCard = ({ nome, descricao, image }) => {
   return (
-    <article className="membro-card">
-      <img style={{borderRadius: 20, boxShadow: "0px 0px 4px rgba(0,0,0,0.5)"}} src={image} alt="" width={200} height={200} />
-      <Tooltip title={descricao} placement="bottom">
+    <Tooltip title={descricao} placement="bottom">
+      <article className="membro-card">
+        <img
+          style={{ borderRadius: 20, boxShadow: "0px 0px 4px rgba(0,0,0,0.5)" }}
+          src={image}
+          alt=""
+          width={200}
+          height={200}
+        />
         <p className="membro-nome">{nome}</p>
-      </Tooltip>
-    </article>
+      </article>
+    </Tooltip>
   );
 };
 
--- src/pages/Eventos/Eventos.jsx
+++ src/pages/Eventos/Eventos.jsx
@@ -1,17 +1,34 @@
 import "./Eventos.css";
 import MainContent from "./../../components/MainContent/MainContent";
-import { Container } from "@mui/material";
 import { useContext } from "react";
 import { myContext } from "../../App";
+import { eventos } from "../Galeria/GaleriaDatas";
+import Container from "../../components/Container/Container";
+import { useNavigate } from "react-router-dom";
 
 const Eventos = () => {
-  const { evento, setEvento } = useContext(myContext);
+  const navigate = useNavigate();
+
+  const { setEvento } = useContext(myContext);
 
   return (
     <MainContent>
       <Container>
         <div className="eventos-flex">
-              
+          {eventos.slice(2).map((evento, eventIndex) => (
+            <div
+              onClick={() => {
+                setEvento(evento.nome);
+                navigate("/galeria");
+              }}
+              key={eventIndex}
+              style={{ backgroundImage: `url(${evento.images[0].link})` }}
+              className="evento-card"
+            >
+              <span className="background-blue-blur"></span>
+              <h2 className="eventos-nome-evento">{evento.nome}</h2>
+            </div>
+          ))}
         </div>
       </Container>
     </MainContent>
