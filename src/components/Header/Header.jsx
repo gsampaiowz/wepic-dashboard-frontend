5a5bf2e5c629aec867ea8c26335df144230feb29
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-12-15 21:18:45 -0300

    Notification data criada

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,35 @@ import "./Header.css";
 import { FaRegBell } from "react-icons/fa";
 import { SlMenu } from "react-icons/sl";
 import fotoPerfil from "../../assets/images/foto_perfil.jpg";
-import MyDropdown from './../Dropdown/Dropdown';
+import MyDropdown from "./../Dropdown/Dropdown";
+import { notifications } from "../../datas/Datas";
 
-const Header = ({ exibeNavbar, setExibeNavbar }) => {
+const Header = ({ setExibeNavbar }) => {
   return (
     <header className="header">
       <div className="header-flex">
-        <MyDropdown titulo={<FaRegBell size={25} />} downArrow={false} menuClass="menu-notification-dropdown"></MyDropdown>
+        <MyDropdown
+          titulo={<FaRegBell size={25} />}
+          downArrow={false}
+          menuClass="menu-notification-dropdown"
+        >
+          {notifications.map((data, index) => (
+            <div key={index} className="notification">
+              <b>{data.title}</b>
+              <small>{data.content}</small>
+            </div>
+          ))}
+        </MyDropdown>
         <img className="foto-perfil" src={fotoPerfil} alt="Foto de perfil" />
         <div className="info">
           <b>Matheus Richard</b>
           <small>Plano A</small>
         </div>
-        <SlMenu className="header__menu__mobile" onClick={() => setExibeNavbar(true)} size={25} />
+        <SlMenu
+          className="header__menu__mobile"
+          onClick={() => setExibeNavbar(true)}
+          size={25}
+        />
       </div>
     </header>
   );
---
+++ src/datas/Datas.js
@@ -0,0 +1,14 @@
+export const notifications = [
+    {
+        title: "Notification 1",
+        content: "Content of notification 1"
+    },
+    {
+        title: "Notification 1",
+        content: "Content of notification 1"
+    },
+    {
+        title: "Notification 1",
+        content: "Content of notification 1"
+    }
+]
